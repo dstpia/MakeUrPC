ae1fca5f437300e8144d98d7a593031ee14cf718
dstpia <ilya14022006@gmail.com>
2026-02-01 15:59:52 +0300

    Base of project

---
+++ src/main/java/com/makeurpc/MakeUrPcApplication.java
@@ -0,0 +1,13 @@
+package com.makeurpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeUrPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeUrPcApplication.class, args);
+	}
+
+}
